f189df1ec55e8e3f3d997f1950c511650d300b08
LG0812 <lg0812@appledeMacBook-Pro.local>
2018-04-30 11:27:03 +0800

    delete package

--- src/generics/Holder.java
+++ src/generics/Holder.java
@@ -1,45 +1,43 @@
 package generics;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-
 public class Holder<T> {
-	private T value;
+    private T value;
 
-	public Holder() {
-	}
+    public Holder() {
+    }
 
-	public Holder(T val) {
-		value = val;
-	}
+    public Holder(T val) {
+        value = val;
+    }
 
-	public void set(T val) {
-		value = val;
-	}
+    public void set(T val) {
+        value = val;
+    }
 
-	public T get() {
-		return value;
-	}
+    public T get() {
+        return value;
+    }
 
-	public boolean equals(Object obj) {
-		return value.equals(obj);
-	}
+    public boolean equals(Object obj) {
+        return value.equals(obj);
+    }
 
-	public static void main(String[] args) {
-		Holder<Apple> apple = new Holder<Apple>(new Apple());
-		Apple d = apple.get();
-		apple.set(d);
-		// Holder<Fruit> fruit = apple;
-		Holder<? extends Fruit> f = apple;
-		Fruit p = f.get();
-		d = (Apple) f.get();
+    public static void main(String[] args) {
+        Holder<Apple> apple = new Holder<Apple>(new Apple());
+        Apple d = apple.get();
+        apple.set(d);
+        // Holder<Fruit> fruit = apple;
+        Holder<? extends Fruit> f = apple;
+        Fruit p = f.get();
+        d = (Apple) f.get();
 
-		try {
-			System.out.println(f.get() instanceof Apple);
-			Orange c = (Orange) f.get();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println(e);
-		}
-		System.out.println(f.equals(d));
-	}
+        try {
+            System.out.println(f.get() instanceof Apple);
+            Orange c = (Orange) f.get();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            System.out.println(e);
+        }
+        System.out.println(f.equals(d));
+    }
 }
